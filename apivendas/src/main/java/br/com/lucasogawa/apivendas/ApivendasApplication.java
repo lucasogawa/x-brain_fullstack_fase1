2e68fe4554ac747f51bd71848fddbac581a165ea
Lucas Akio Ogawa <lucasogawa@MacBook-Air-de-Lucas.local>
2019-10-11 13:43:16 -0300

    projeto criado

---
+++ apivendas/src/main/java/br/com/lucasogawa/apivendas/ApivendasApplication.java
@@ -0,0 +1,13 @@
+package br.com.lucasogawa.apivendas;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ApivendasApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ApivendasApplication.class, args);
+	}
+
+}
